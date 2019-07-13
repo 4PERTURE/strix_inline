c32eb382c8191372b50d718167cd13f88767b694
Keternal <Z5X67280@163.com>
2019-07-13 23:31:05 +0800

    proc: Add a temporal workaround for set_top_app()
    - MIUI uses a node under /proc/ called top_app to store whether a pid is a
    top_app's pid. This is a temporal workaroud for it.
    - It can mute "Couldn't find top_app" warning
    - Idea from c01b4365254fdb5284824f5a1b6ff64fdb08f879
    
    Signed-off-by: Keternal <Z5X67280@163.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2257,6 +2257,7 @@ struct task_struct {
 	/* A live task holds one reference. */
 	atomic_t stack_refcount;
 #endif
+	int		top_app;
 /* CPU-specific state of this task */
 	struct thread_struct thread;
 /*
