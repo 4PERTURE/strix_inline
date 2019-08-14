2cb0744505baeb5ac7498e2d5b96037f42de6d4c
Nathan Chancellor <natechancellor@gmail.com>
2019-08-14 09:33:57 -0700

    Merge tag 'LA.UM.7.6.2.r1-09500-89xx.0' into kernel.lnx.4.9.r12-rel
    "LA.UM.7.6.2.r1-09500-89xx.0"
    
    * tag 'LA.UM.7.6.2.r1-09500-89xx.0':
      diag: Prevent out-of-bound access while processing dci transaction
      msm: camera: jpegdma: Added missing lock for dqbuf and streamon
      msm: kgsl: Fix race condition between drawobj and context destroy
      soc: qcom: glink_smem: Initialize buffers
      msm: camera: eeprom: Fix OOB condition for memory map count
      msm:camera:isp: Fix array index bound checks
      msm: vidc: Add checks to avoid OOB access
      msm: camera: eeprom: Fix OOB read/write in EEPROM
      msm: camera: icp: Prevent out of bound access in acquire
      msm: camera_v3: eeprom: Fix OOB condition for memory map count
      msm: camera: eeprom: Fix OOB read/write in EEPROM
      msm: vidc: Avoid information leak while accessing the packet
      f2fs: fix to recover inode's i_gc_failures during POR
      f2fs: fix to initialize i_current_depth according to inode type
      f2fs: fix to reset i_gc_failures correctly
      PM / devfreq: gpubw_mon: Add null check for governor private data
      fbdev: msm: check the length of the external input buffer properly
      soc: qcom: Fix blackghost status irq missing during BG SSR
      Merge remote-tracking branch 'dev/msm-4.14' into msm-4.9 06/12
      ARM: dts: msm: Increasing shared mem region size for SDM845 and SDM710
      msm: camera: icp: Send I/O memory region info to FW
      net: sockev: avoid races between sockev and socket_close
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -274,6 +274,8 @@ static int recover_inode(struct inode *inode, struct page *page)
 	f2fs_set_inode_flags(inode);
 	F2FS_I(inode)->i_gc_failures[GC_FAILURE_PIN] =
 				le16_to_cpu(raw->i_gc_failures);
+	F2FS_I(inode)->i_gc_failures =
+			le16_to_cpu(raw->i_gc_failures);
 
 	recover_inline_flags(inode, raw);
 
