d02ef8423e900c51d16e0041ac190f6ec5dedfd4
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2019-09-15 15:17:52 +0700

    Revert: "net: core: Sample RX packets after RPS processing"
    The internet is getting a little slow
    
    This reverts commit https://github.com/fiqri14082019/kernel_xiaomi_vince-4.9/commit/bf784709cd2b61dc914e14bab4183c058150814d
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3592,7 +3592,7 @@ EXPORT_SYMBOL(dev_queue_xmit_list);
 int netdev_max_backlog __read_mostly = 1000;
 EXPORT_SYMBOL(netdev_max_backlog);
 
-int netdev_tstamp_prequeue __read_mostly;
+int netdev_tstamp_prequeue __read_mostly = 1;
 int netdev_budget __read_mostly = 300;
 int weight_p __read_mostly = 64;            /* old backlog weight */
 
