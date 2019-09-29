tytydraco/android_kernel_xiaomi_jasmine

HACK: mm: add Kconfig interface for vmstat interval
Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

mm/vmstat.c
@@ -1386,7 +1386,7 @@ static const struct file_operations proc_vmstat_file_operations = {
 #ifdef CONFIG_SMP
 static struct workqueue_struct *vmstat_wq;
 static DEFINE_PER_CPU(struct delayed_work, vmstat_work);
-int sysctl_stat_interval __read_mostly = HZ;
+int sysctl_stat_interval __read_mostly = HZ * CONFIG_VM_STAT_INTERVAL;
 static cpumask_var_t cpu_stat_off;
 
 static void vmstat_update(struct work_struct *w)

4PERTURE/strix_inline

Signed-off-by: kdrag0n <dragon@khronodragon.com>
Signed-off-by: laststandrighthere <laststandrighthere@gmail.com>

mm/vmstat.c
@@ -1558,7 +1558,7 @@ static const struct file_operations proc_vmstat_file_operations = {
 #ifdef CONFIG_SMP
 static struct workqueue_struct *vmstat_wq;
 static DEFINE_PER_CPU(struct delayed_work, vmstat_work);
-int sysctl_stat_interval __read_mostly = HZ;
+int sysctl_stat_interval __read_mostly = CONFIG_VMSTAT_INTERVAL * HZ;
 
 #ifdef CONFIG_PROC_FS
 static void refresh_vm_stats(struct work_struct *work)